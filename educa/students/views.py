05d541b79b8e9191d5ad5f3d3a5a7b9334fc5a58
Kiraogi <golokron3@yandex.ru>
2025-09-16 20:42:27 +0300

    ver 0.01.38.01
    `Update template names in student views to 'students/' prefix`

--- educa/students/views.py
+++ educa/students/views.py
@@ -38,7 +38,7 @@ class StudentEnrollCourseView(LoginRequiredMixin, FormView):
 
 class StudentCourseListView(LoginRequiredMixin, ListView):
     model = Course
-    template_name = 'student/course/list.html'
+    template_name = 'students/course/list.html'
 
     def get_queryset(self):
         qs = super().get_queryset()
@@ -46,7 +46,7 @@ class StudentCourseListView(LoginRequiredMixin, ListView):
 
 class StudentCourseDetailView(DetailView):
     model = Course
-    template_name = 'student/course/detail.html'
+    template_name = 'students/course/detail.html'
 
     def get_queryset(self):
         qs = super().get_queryset()
