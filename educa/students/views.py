91633857fec67ec861856027cbd288a58f31f926
Kiraogi <golokron3@yandex.ru>
2025-09-16 19:54:15 +0300

    ver 0.01.36
    "Added StudentCourseListView and modified StudentEnrollCourseView to filter courses by enrolled students."

--- educa/students/views.py
+++ educa/students/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic.edit import CreateView, FormView
+from django.views.generic.list import ListView
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.mixins import LoginRequiredMixin
 
+
 from .forms import CourseEnrollForm
+from courses.models import Course
 
 class StudentRegistrationView(CreateView):
     template_name = 'students/student/registration.html'
@@ -32,4 +35,10 @@ class StudentEnrollCourseView(LoginRequiredMixin, FormView):
     def get_success_url(self):
         return reverse_lazy('student_course_detail', args=[self.course.id])
 
+class StudentCourseListView(LoginRequiredMixin, ListView):
+    model = Course
+    template_name = 'student/course/list.html'
 
+    def get_queryset(self):
+        qs = super().get_queryset()
+        return qs.filter(students__in=[self.request.user])
