09c23d104f85b62d56a80b8e8843fe9cbe7b19de
Kiraogi <golokron3@yandex.ru>
2025-09-23 20:51:41 +0300

    ver 0.01.39
    `Added render method to ItemBase model and imported django template loader`

--- educa/courses/models.py
+++ educa/courses/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes.fields import GenericForeignKey
+from django.template.loader import render_to_string
 
 from .fields import OrderField
 
@@ -74,6 +75,10 @@ class ItemBase(models.Model):
     title = models.CharField(max_length=250)
     created = models.DateTimeField(auto_now_add=True)
     update = models.DateTimeField(auto_now=True)
+
+    def render(self):
+        return render_to_string(f'courses/content/{self._meta.model_name}.html',
+                                {'item': self})
 
     class Meta:
         abstract = True
