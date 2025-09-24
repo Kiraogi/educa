69dce9ac43c98832fcb70b8dbb9eed4733acb33b
Kiraogi <golokron3@yandex.ru>
2025-09-24 21:40:49 +0300

    ver 0.01.41
    `Refactor embed_video_tags and CourseEnrollForm in educa app`

--- educa/students/views.py
+++ educa/students/views.py
@@ -26,14 +26,11 @@ class StudentRegistrationView(CreateView):
 
 class StudentEnrollCourseView(LoginRequiredMixin, FormView):
     course = None
-    form_class = CourseEnrollForm
-    
-
+    form_class = CourseEnrollForm    
     def form_valid(self, form):
         self.course = form.cleaned_data['course']
         self.course.students.add(self.request.user)
         return super().form_valid(form)
-    
     def get_success_url(self):
         return reverse_lazy('student_course_detail', args=[self.course.id])
 
