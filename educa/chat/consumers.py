6b05a1cefa70233f0c4a6f49b02ccf6a99148d2a
Kiraogi <golokron3@yandex.ru>
2026-02-23 16:02:53 +0300

    ver 0.01.58
    `Updated ChatConsumer to use AsyncWebsocketConsumer and added user information to chat messages`

--- educa/chat/consumers.py
+++ educa/chat/consumers.py
@@ -1,27 +1,46 @@
 import json
-from channels.generic.websocket import WebsocketConsumer
+from channels.generic.websocket import AsyncWebsocketConsumer
 from asgiref.sync import async_to_sync
+from django.utils import timezone
 
-class ChatConsumer(WebsocketConsumer):
-    def connect(self):
+class ChatConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        self.user = self.scope['user']
         self.id = self.scope['url_route']['kwargs']['course_id']
         self.room_group_name = f'chat_{self.id}'
         # Присоединиться к группе чат-комнаты
-        async_to_sync(self.channel_layer.group_add)(
+        await self.channel_layer.group_add(
             self.room_group_name, 
             self.channel_name
             )
-        # инять соединение
-        self.accept()
+        # Принять соединение
+        await self.accept()
     
-    def disconnect(self, close_code):
-        pass
+    async def disconnect(self, close_code):
+        # Покинуть группу чат-комнаты
+        await self.channel_layer.group_discard(
+            self.room_group_name,
+            self.channel_name
+        )
 
     # Получить сообщение из WebSocket
-    def receive(self, text_data):
-        text_data_json = json.load(text_data)
-        message = text_data_json
-        # Отправить сообщение в WebSocket
-        self.send(text_data=json.dumps({'message': message}))
+    async def receive(self, text_data):
+        text_data_json = json.loads(text_data)
+        message = text_data_json['message']
+        now = timezone.now()
+        # Отправить сообщение в группу чат-комнат
+        await self.channel_layer.group_send(
+            self.room_group_name,
+            {
+                'type': 'chat_message',
+                'message': message,
+                'user': self.user.username,
+                'datetime': now.isoformat(),
+            }
+        )
+    
+    # Получить сообщение из группы
+    async def chat_message(self, event):
+        # Отправить сообщение в вебсокет
+        await self.send(text_data=json.dumps(event))
 
-        
