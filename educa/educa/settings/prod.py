17ffb269c8ed55f75169f8b890dcbedcb995a252
Kiraogi <golokron3@yandex.ru>
2026-02-25 19:46:07 +0300

    ver 0.01.64
    `Update Nginx configuration and Django settings for subdomain support`

--- educa/educa/settings/prod.py
+++ educa/educa/settings/prod.py
@@ -7,7 +7,7 @@ ADMINS = [
     ('admin', 'admin@example.com'),
 ]
 
-ALLOWED_HOSTS = ['educaproject.com', 'www.educaproject.com']
+ALLOWED_HOSTS = ['.educaproject.com']
 
 DATABASES = {
     'default': {
